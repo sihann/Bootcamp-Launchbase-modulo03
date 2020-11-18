f5757c0194a1f6eca1e7fb55bd2d1120eaad2dc5
Delania Oliveira <fkydebys@gmail.com>
2020-11-18 16:40:08 -0300

    fix: script

--- public/modal.js
+++ public/modal.js
@@ -1,8 +1,4 @@
-const iframe = document.querySelector('.iframe');
 const cards = document.querySelectorAll('.card');
-const iframeModal = document.querySelector('.iframe-modal');
-const maxIframe = document.querySelector('.max-iframe');
-const footer = document.querySelector('footer');
 
 // adiciona uma class em todos os cards
 for (let card of cards) {
